7fdf2f129fedfd314af954d3f4eb8c4615c643d6
Jamie Le Brun <j.lebrun9@gmail.com>
2021-03-27 21:33:45 +0000

    setup efm and eslint for tsx linting

--- lua/custom/efm/eslint.lua
+++ lua/custom/efm/eslint.lua
@@ -1,8 +1,8 @@
 return {
-  lintCommand = 'eslint_d --stdin --stdin-filename ${INPUT} -f unix',
+  lintCommand = 'npx eslint --stdin --stdin-filename ${INPUT} -f unix',
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %m"},
   lintIgnoreExitCode = true,
-  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatCommand = "npx eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
   formatStdin = true
 }
