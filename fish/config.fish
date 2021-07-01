f018f0768d101b492831e4975c319847bcd97e92
Jamie Le Brun <j.lebrun9@gmail.com>
2021-07-01 20:46:12 +0100

    Feat: added function doom to fish
    - Starts Emacs daemon and opens a emacs client attached to the running
    server
    - Also removed the abbreviation from the previous commit which did not
    work as was intended

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,11 @@ neofetch
 abbr -a rc rails console
 abbr -a rs rails server
 abbr -a wp ./bin/webpack-dev-server
-abbr -a doom emacs --daemon && emacsclient --no-wait --create-frame
+
+function doom -d "Start the Emacs daemon and opens the Emacs Client"
+    command emacs --daemon
+    command emacsclient --no-wait --create-frame
+end
 
 set PATH /usr/local/opt/mysql@5.7/bin $PATH
 
