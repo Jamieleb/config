ad19802176f4d5fffd6dfbd1d7f2b09a1f8df47d
Jamie <j.lebrun9@gmail.com>
2021-09-15 16:58:37 +0100

    transition majority of lsp.vim to lsp.lua

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,44 @@
+vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
+vim.api.nvim_set_var('completion_enable_auto_popup', 1)
+vim.api.nvim_set_var('completion_matching_strategy_list', {'exact', 'substring', 'fuzzy'})
+vim.api.nvim_set_var('completion_chain_complete_list', {
+  { complete_items =  {'lsp'} },
+  { complete_items =  {'buffers', 'path'} },
+  { mode =  '<c-p>' },
+  { mode =  '<c-n>' },
+})
+vim.api.nvim_set_var('completion_auto_change_source', 1)
+
+local saga = require('lspsaga')
+saga.init_lsp_saga()
+
+local wk = require('which-key')
+wk.register({
+  l = {
+    name = 'lsp',
+    d = { '<cmd>lua require("lspsaga.provider").preview_definition()<CR>', 'preview definition' },
+    D = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'go to definition' },
+    r = { '<cmd>Trouble lsp_references', 'show references in Trouble' },
+    R = { '<cmd>lua require("lspsaga.rename").rename()<CR>', 'rename symbol' },
+    k = { '<cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', 'show documentation' },
+    e = { '<cmd>lua require("lspsaga.diagnostic").show_line_diagnostics()<CR>', 'show line diagnostics' },
+    n = { '<cmd>lua require("lspsaga.diagnostic").lsp_jump_diagnostic_next()<CR>', 'next diagnostic' },
+    p = { '<cmd>lua require("lspsaga.diagnostic").lsp_jump_diagnostic_prev()<CR>', 'previous diagnostic' },
+    l = { '<cmd>Trouble lsp_document_diagnostics<CR>', 'Trouble document diagnostics' },
+    L = { '<cmd>Trouble lsp_workspace_diagnostics<CR>', 'Trouble workspace diagnostics' },
+    a = { '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', 'code actions' },
+    s = { '<cmd>Telescope lsp_document_symbols<CR>', 'search document symbols' },
+    S = { '<cmd>Telescope lsp_workspace_symbols<CR>', 'search workspace symbols' },
+    i = { '<cmd>Telescope lsp_implementations<CR>', 'search implementations' },
+    f = { '<cmd>Lspsaga lsp_finder<CR>', 'lsp finder' },
+    x = { '<cmd>ISwap<CR>', 'swap args' },
+    X = { '<cmd>ISwapWith<CR>', 'swap arg under cursor' },
+  }
+}, { prefix = '<leader>' })
+wk.register({
+  a = { ':<C-u>lua require("lspsaga.codeaction").range_code_action()<CR>', 'range code action' },
+}, { prefix = "<leader>l", mode = 'v' })
+wk.register({
+  ['<C-f>'] = { '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', 'saga smart scroll forward' },
+  ['<C-b>'] = { '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(-1)<CR>', 'saga smart scroll backward' }
+})
--- nvim/plugin/lsp.vim
+++ nvim/plugin/lsp.vim
@@ -1,49 +1,5 @@
 " LSP & Completion
-set completeopt=menuone,noinsert,noselect
-let g:completion_enable_auto_popup = 1
-let g:completion_matching_strategy_list = ['exact', 'substring', 'fuzzy']
-
-let g:completion_chain_complete_list = [
-  \{'complete_items': ['lsp']},
-  \{'complete_items': ['buffers', 'path']},
-  \{'mode': '<c-p>'},
-  \{'mode': '<c-n>'}
-\]
-
-let g:completion_auto_change_source = 1
-
-" Enable completion in every buffer
-" autocmd BufEnter * lua require'completion'.on_attach()
-
-lua << EOF
-local saga = require 'lspsaga'
-
-saga.init_lsp_saga()
-EOF
-
-nnoremap <silent><leader>ld :Lspsaga preview_definition<CR>
-nnoremap <silent><leader>lD :lua vim.lsp.buf.definition()<CR>
-nnoremap <silent><leader>lr :Trouble lsp_references<CR>
-nnoremap <silent><leader>lR :Lspsaga rename<CR>
-nnoremap <silent><leader>lk :Lspsaga hover_doc<CR>
-nnoremap <silent><leader>le :Lspsaga show_line_diagnostics<CR>
-nnoremap <silent><leader>ln :Lspsaga diagnostic_jump_next<CR>
-nnoremap <silent><leader>lp :Lspsaga diagnostic_jump_previous<CR>
-nnoremap <silent><leader>ll :Trouble lsp_document_diagnostics<CR>
-nnoremap <silent><leader>lL :Trouble lsp_workspace_diagnostics<CR>
-nnoremap <silent><leader>la :Lspsaga code_action<CR>
-vnoremap <silent><leader>la :<C-U>Lspsaga range_code_action<CR>
-nnoremap <silent><leader>lc :lua vim.lsp.stop_client(vim.lsp.get_active_clients())
-nnoremap <silent><leader>ls :Telescope lsp_document_symbols<CR>
-nnoremap <silent><leader>lS :Telescope lsp_workspace_symbols<CR>
-nnoremap <silent><leader>li :Telescope lsp_implementations<CR>
-nnoremap <silent><leader>lf :Lspsaga lsp_finder<CR>
-nnoremap <silent><Leader>lx :ISwap<CR>
-nnoremap <silent><Leader>lX :ISwapWith<CR>
-
-" Scroll hover docs
-nnoremap <silent><C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
-nnoremap <silent><C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
+lua require('lsp')
 
 " Use <Tab> and <S-Tab> to navigate through popup menu
 inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
