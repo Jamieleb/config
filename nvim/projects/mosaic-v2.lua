c7ad30a07fb1ddc6ddb3f9f1bca04886792b822e
Jamie <j.lebrun9@gmail.com>
2021-11-12 10:56:19 +0000

    add job worker toggle

--- nvim/projects/mosaic-v2.lua
+++ nvim/projects/mosaic-v2.lua
@@ -1,37 +1,63 @@
-local Terminal = require('toggleterm.terminal').Terminal
-local console = Terminal:new({ cmd = 'rails c', hidden = true, close_on_exit = true })
-local server = Terminal:new({ cmd = 'rails s', hidden = true, close_on_exit = true })
-local webpack = Terminal:new({ cmd = 'nvm use && yarn && ./bin/webpack-dev-server', hidden = true, close_on_exit = true })
-local custom_tele = require('custom.telescope')
+local Terminal = require("toggleterm.terminal").Terminal
+local console = Terminal:new({ cmd = "rails c", hidden = true, close_on_exit = true })
+local server = Terminal:new({ cmd = "rails s", hidden = true, close_on_exit = true })
+local webpack = Terminal:new({
+	cmd = "nvm use && yarn && ./bin/webpack-dev-server",
+	hidden = true,
+	close_on_exit = true,
+})
+local start_worker = Terminal:new({ cmd = "rails jobs:work", hidden = true, close_on_exit = true })
+clear_jobs = Terminal:new({ cmd = "rails jobs:clear", hidden = true, close_on_exit = true })
+local custom_tele = require("custom.telescope")
 
-function _rails_models() custom_tele.search_path('./app/models/', 'Models') end
-function _rails_controllers() custom_tele.search_path('./app/controllers/', 'Controllers') end
-function _rails_views() custom_tele.search_path('./app/views/', 'Views') end
-function _rails_services() custom_tele.search_path('./app/services/', 'Services') end
-function _rails_resources() custom_tele.search_path('./app/resources/', 'Resourcees') end
-function _rails_components() custom_tele.search_path('./app/webpacker/components/', 'Services') end
+function _rails_models()
+	custom_tele.search_path("./app/models/", "Models")
+end
+function _rails_controllers()
+	custom_tele.search_path("./app/controllers/", "Controllers")
+end
+function _rails_views()
+	custom_tele.search_path("./app/views/", "Views")
+end
+function _rails_services()
+	custom_tele.search_path("./app/services/", "Services")
+end
+function _rails_resources()
+	custom_tele.search_path("./app/resources/", "Resourcees")
+end
+function _rails_components()
+	custom_tele.search_path("./app/webpacker/components/", "Services")
+end
 
 function _console_toggle()
-  console:toggle()
+	console:toggle()
 end
 
 function _server_toggle()
-  server:toggle()
+	server:toggle()
 end
 
 function _webpack_toggle()
-  webpack:toggle()
+	webpack:toggle()
 end
 
-local wk = require('which-key')
+function _toggle_worker()
+	start_worker:toggle()
+end
+
+local wk = require("which-key")
 wk.register({
-  c = { '<cmd>lua _console_toggle()<CR>', 'toggle rails console' },
-  s = { '<cmd>lua _server_toggle()<CR>', 'toggle rails server' },
-  w = { '<cmd>lua _webpack_toggle()<CR>', 'toggle wepback-dev-server' },
-  M = { '<cmd>lua _rails_models()<CR>', 'search models' },
-  C = { '<cmd>lua _rails_controllers()<CR>', 'search controllers' },
-  R = { '<cmd>lua _rails_components()<CR>', 'search React components' },
-  V = { '<cmd>lua _rails_views()<CR>', 'search views' },
-  S = { '<cmd>lua _rails_services()<CR>', 'search services' },
-  G = { '<cmd>lua _rails_resources()<CR>', 'search graphiti resources' },
-}, { prefix = '<leader>.' })
+	c = { "<cmd>lua _console_toggle()<CR>", "toggle rails console" },
+	s = { "<cmd>lua _server_toggle()<CR>", "toggle rails server" },
+	w = { "<cmd>lua _webpack_toggle()<CR>", "toggle wepback-dev-server" },
+	M = { "<cmd>lua _rails_models()<CR>", "search models" },
+	C = { "<cmd>lua _rails_controllers()<CR>", "search controllers" },
+	R = { "<cmd>lua _rails_components()<CR>", "search React components" },
+	V = { "<cmd>lua _rails_views()<CR>", "search views" },
+	S = { "<cmd>lua _rails_services()<CR>", "search services" },
+	G = { "<cmd>lua _rails_resources()<CR>", "search graphiti resources" },
+	j = { "<cmd>lua _toggle_worker()<CR>", "toggle jobs worker" },
+	J = { "<cmd>lua clear_jobs()<CR>", "clear jobs" },
+}, {
+	prefix = "<leader>.",
+})
