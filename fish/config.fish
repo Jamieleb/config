c860bed8c98eb883d6300c137d361b784b68313e
Jamie Le Brun <jamie.lebrun@vaiie.com>
2024-01-10 09:32:12 +0000

    chore: small fish & alfred updates

--- fish/config.fish
+++ fish/config.fish
@@ -5,18 +5,14 @@ fish_vi_key_bindings
 set -g fish_greeting
 
 # Abbreviations
-abbr -a rc rails console
-abbr -a rs rails server
-abbr -a wp ./bin/webpack-dev-server
-abbr -a nv nvim
+abbr -a nv lvim
 abbr -a icloud ~/Library/Mobile\ Documents/com~apple~CloudDocs
 
-function doom -d "Start the Emacs daemon and opens the Emacs Client"
-    command emacs --daemon
-    command emacsclient --no-wait --create-frame
-end
-
-fish_add_path /opt/homebrew/opt/mysql@5.7/bin
+abbr -a za zellij attach
+abbr -a zc zellij attach -c
+abbr -a zl zellij ls
+ 
+abbr -a ff tmux-sessionizer
 
 status --is-interactive; and source (rbenv init -|psub)
 fnm env --use-on-cd | source
@@ -27,5 +23,11 @@ fish_add_path /opt/homebrew/opt/openssl@1.1/bin
 fish_add_path $HOME/Library/Python/3.8/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/config/scripts
 
 export GPG_TTY=$(tty)
+
+zoxide init fish | source
+
+# opam configuration
+source /Users/jamie.lebrun/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
---
+++ fish/functions/br.fish
@@ -0,0 +1,1 @@
+/Users/jamie.lebrun/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
